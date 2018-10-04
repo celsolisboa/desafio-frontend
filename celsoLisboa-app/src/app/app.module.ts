7745e86102068abcc9f0d0d4932cabc53e4b9be4
Diego D. Costa <diegocostadeveloper@gmail.com>
2018-10-03 21:15:09 -0300

    Adicionado: - Queries de css - Component header - Tela de login mobile (apenas html + css)

--- celsoLisboa-app/src/app/app.module.ts
+++ celsoLisboa-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
