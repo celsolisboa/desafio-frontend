abb4fd0acd7ca74bf3bfddd454718d5798c7a1ea
Diego D. Costa <diegocostadeveloper@gmail.com>
2018-10-04 12:18:54 -0300

    start component cursos

--- celsoLisboa-app/src/app/app.module.ts
+++ celsoLisboa-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HeaderComponent,
+    CursosComponent,
   ],
   imports: [
     BrowserModule
