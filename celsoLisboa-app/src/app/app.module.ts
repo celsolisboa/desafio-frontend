5a36c07f7f474d298884cf56f643b077884ba1e9
Diego D. Costa <diegocostadeveloper@gmail.com>
2018-10-08 15:21:27 -0300

    component login e limpeza dos scss e spec dos outros components

--- celsoLisboa-app/src/app/app.module.ts
+++ celsoLisboa-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { CoursesComponent } from './courses/courses.component';
     LoginComponent,
     HeaderComponent,
     CoursesComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
--- celsoLisboa-app/src/app/courses/courses.component.ts
+++ celsoLisboa-app/src/app/courses/courses.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  templateUrl: './courses.component.html'
 })
 export class CoursesComponent implements OnInit {
 
