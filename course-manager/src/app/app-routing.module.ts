df4b4180626f65f76ce8fc379e8fdc16899001d3
Ricardo Cardoso <rickclsilva@gmail.com>
2018-10-23 10:27:32 -0300

    :bug: fix routes

--- course-manager/src/app/app-routing.module.ts
+++ course-manager/src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CoursesComponent } from './courses/courses.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/cursos',
+    redirectTo: '/login',
     pathMatch: 'full'
   },
   {
     path: 'login',
     component: LoginComponent
   },
-  // {
-  //   path: 'cursos',
-  //   component: CoursesComponent
-  // }
+  {
+    path: 'cursos',
+    component: CoursesComponent
+  }
 ];
 
 @NgModule({
