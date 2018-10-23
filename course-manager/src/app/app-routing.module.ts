903ade14ec9e3bab275e1da9f7a2288639207d74
Ricardo Cardoso <rickclsilva@gmail.com>
2018-10-23 09:44:44 -0300

    :sparkles: add basic components

--- course-manager/src/app/app-routing.module.ts
+++ course-manager/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/cursos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  // {
+  //   path: 'cursos',
+  //   component: CoursesComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
