c9688f682d2046dc280b950bc27cd62451a70c23
Igor Imperiali <igor.imperiali@gmail.com>
2018-10-27 18:31:50 -0200

    Criacao de component home
    Compente home sera responsavel pelos cursos em si
    Definicao de layout
    Gerenciamento de rotas

--- courseManager/src/app/app.component.ts
+++ courseManager/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'courseManager';
+  logado = false;
+  constructor(){
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  login(){
+    this.logado = true;
+  }
 }
--- courseManager/src/app/app.module.ts
+++ courseManager/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
