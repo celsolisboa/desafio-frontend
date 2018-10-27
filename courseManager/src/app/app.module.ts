36584e877a5c271009f6cd70b5f1180f0e5bb58d
Igor Imperiali <igor.imperiali@gmail.com>
2018-10-27 18:54:07 -0200

    Capturando cursos da api

--- courseManager/src/app/app.module.ts
+++ courseManager/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +13,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- courseManager/src/app/home/home.component.ts
+++ courseManager/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from "rxjs/index";
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  cursos: any;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCursos():Observable<any>{
+    return this.http.get<any>("http://localhost:3000/api/curso")
+  }
 
   ngOnInit() {
-
+    this.getCursos().subscribe(data =>{
+      this.cursos = data.cursos
+      console.log(this.cursos)
+    })
   }
 
 }
