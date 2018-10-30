d9dbdbf6c59b0b205d732b26cfaed0926cebd6c6
LuAnderson <lucasand96@gmail.com>
2018-10-29 23:48:06 -0300

    validando login de forma fracamente tipada

--- js/script.js
+++ js/script.js
@@ -14,15 +14,27 @@ app.controller('loginController', ['$scope', '$http', function ($scope, $http) {
     //     'password': 'test123'
     // }]
 
-    $http({
-        method: 'POST',
-        url: baseUrl,
-        data: { email: "john@gmail.com", password: "passwd" }
-    }).then(function successCallback(response) {
-        console.log("logou", response)
-        console.log()
-    }, function errorCallback(response) {
-        console.log("falhou", response)
-    });
+    $scope.loginProfile = function () {
+
+        if ($scope.accessEmail == "john@gmail.com" ||
+            $scope.accessEmail == "bill@gmail.com") {
+            authentication($scope.accessEmail, $scope.accessPassword);
+        }
+    }
+
+    function authentication(email, password) {
+        if (email == "john@gmail.com" && password == "passwd" ||
+            email == "bill@gmail.com" && password == "test123") {
+            $http({
+                method: 'POST',
+                url: baseUrl,
+                data: { email: email, password: password }
+            }).then(function successCallback(response) {
+                console.log("logou", response)
+            }, function errorCallback(response) {
+                console.log("falhou", response)
+            });
+        }
+    }
 
 }]);
