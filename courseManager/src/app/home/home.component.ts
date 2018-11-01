fb791ef55ca0c71cb1de6beb441b27c4ae3800ba
Igor Imperiali <igor.imperiali@gmail.com>
2018-11-01 20:15:19 -0200

    Concluindo crud basico

--- courseManager/src/app/home/home.component.ts
+++ courseManager/src/app/home/home.component.ts
@@ -24,12 +24,29 @@ export class HomeComponent implements OnInit {
   salas: [];
   inicio: '';
   fim:'';
+  atualiza = false;
   constructor(
     private http: HttpClient
   ) { }
 
-  getCursos():Observable<any>{
+  getCursos(){
     return this.http.get<any>("http://localhost:3000/api/curso")
+  }
+
+  postCurso(curso){
+    return this.http.post<any>("http://localhost:3000/api/curso", curso)
+  }
+
+  putCurso(curso){
+    return this.http.patch<any>(`http://localhost:3000/api/curso/${curso.id}`, curso)
+  }
+
+  getProfs(){
+    return this.http.get<any>("http://localhost:3000/api/professor")
+  }
+
+  getSalas(){
+    return this.http.get<any>("http://localhost:3000/api/sala")
   }
 
   delCursos(id){
@@ -41,9 +58,33 @@ export class HomeComponent implements OnInit {
       this.cursos = data.cursos
       console.log(this.cursos)
     })
+    this.getProfs().subscribe( data => {
+      this.professores = data
+    })
+    this.getSalas().subscribe( data => {
+      this.salas = data
+    })
   }
 
   cadastraCurso(){
+    if (!this.atualiza){
+      this.postCurso(this.novoCurso).subscribe( data => {
+        alert('Cadastrado com sucesso!')
+      }, error => {
+        alert('Algo deu errado!')
+      })
+    }else{
+      this.putCurso(this.novoCurso).subscribe( data => {
+        alert('Atualizado com sucesso!')
+      }, error => {
+        alert('Algo deu errado!')
+      })
+    }
+  }
+
+  atualizaCurso(id){
+    this.novoCurso = Object.assign({}, this.cursos.filter(x => x.id == id)[0])
+    this.atualiza = true
     console.log(this.novoCurso)
   }
 
@@ -51,9 +92,39 @@ export class HomeComponent implements OnInit {
     let curso = this.cursos.filter(x => x.id == id)
     console.log(curso)
     confirm(`Tem certeza que deseja deletar o curso ${curso[0].nome} ?`) ?
-    this.delCursos(id).subscribe( data => {
-      console.log(data)
-    }) : false
+      this.delCursos(id).subscribe( data => {
+        console.log(data)
+      }) : false
   }
+
+  limpaCampos(){
+    for(let key in this.novoCurso){
+      if(this.novoCurso.hasOwnProperty(key)){
+        if(typeof this.novoCurso[key] === 'string'){
+          this.novoCurso[key] = ''
+        }else if(this.novoCurso[key] instanceof Array){
+          this.novoCurso[key] = []
+        }
+      }
+    }
+    this.atualiza = false;
+  }
+
+  cadastraSalas(options){
+    this.novoCurso.salas = Object.assign(Array.apply(null, options)
+      .filter(options => options.selected)
+      .map(options => options.value))
+
+    console.log(this.novoCurso.salas)
+  }
+
+  cadastraProfs(options){
+    this.novoCurso.professores = Object.assign(Array.apply(null, options)
+      .filter(options => options.selected)
+      .map(options => options.value))
+
+    console.log(this.novoCurso.professores)
+  }
+
 
 }
