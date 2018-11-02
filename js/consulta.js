dcdfaae947ac4d7bc8d0b5859f06f8486ca563d2
LuAnderson <lucasand96@gmail.com>
2018-11-02 17:49:38 -0300

    item 4 - visualização e remoção de cursos

--- js/consulta.js
+++ js/consulta.js
@@ -3,22 +3,34 @@ var app = angular.module('celsoLisboa', []);
 app.controller('consultaController', ['$scope', '$http', function ($scope, $http) {
 
     function detectedErro(e) {
-        console.log("e:", e);
+        console.log("Erro encontrado:", e);
     }
 
-    function getAPI() {
+    function getCurso() {
         $http.get("http://localhost:3000/api/curso")
             .then(function (response) {
                 $scope.infoCurso = response.data;
-
-                $scope.t = $scope.infoCurso.cursos;
-                console.log("$scope.t", $scope.t);
                 $scope.infoCurso = $scope.infoCurso.cursos;
 
             }, function myError(response) {
                 detectedErro("Foi detectada um falha na conexão com a API (Cursos): ", response)
             });
     }
-    getAPI();
+    getCurso();
+
+    $scope.removeCurso = function (posicao) {
+        $http.delete('http://localhost:3000/api/curso/' + posicao)
+            .then(function () {
+                console.log("Curso Removido!");
+                getCurso();
+
+            }, function (response) {
+                detectedErro(response)
+            });
+    }
+
+    $scope.addCurso = function(){
+
+    }
 
 }]);
