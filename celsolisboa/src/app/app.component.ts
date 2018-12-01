92ac2ef24d4d558a61ada6a23518ad33388c6d03
Vanessa Mello <vanessa.mello.viana@gmail.com>
2018-11-30 22:14:25 -0200

    Criação da tela de Login

--- celsolisboa/src/app/app.component.ts
+++ celsolisboa/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './login/login.component.html',
+  styleUrls: ['./login/login.component.css', './app.component.css']
 })
 export class AppComponent {
   title = 'Celso Lisboa';
