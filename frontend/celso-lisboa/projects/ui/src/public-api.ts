e4a2690955c0c932998b62c75ee1a0c9a0f23eb3
Brendon Costa <43861825+brendon-costa@users.noreply.github.com>
2020-11-20 15:24:49 -0300

    feat(cl-input): add input in api

--- frontend/celso-lisboa/projects/ui/src/public-api.ts
+++ frontend/celso-lisboa/projects/ui/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ui
  */
 
-// export * from './lib/ui.service';
-// export * from './lib/ui.component';
-// export * from './lib/ui.module';
+export * from './lib/form-controls/cl-input/cl-input.component';
+export * from './lib/form-controls/cl-input/cl-input.module';
