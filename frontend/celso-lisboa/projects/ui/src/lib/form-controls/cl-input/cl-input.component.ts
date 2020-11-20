686ec057e0fb30b4baa6a7aa8c91562a2d96ba17
Brendon Costa <43861825+brendon-costa@users.noreply.github.com>
2020-11-20 17:01:05 -0300

    feat(cl-input): create ui

--- frontend/celso-lisboa/projects/ui/src/lib/form-controls/cl-input/cl-input.component.ts
+++ frontend/celso-lisboa/projects/ui/src/lib/form-controls/cl-input/cl-input.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {fromEvent, Subscription} from 'rxjs';
 
 @Component({
   selector: 'ui-cl-input',
   templateUrl: './cl-input.component.html',
   styleUrls: ['./cl-input.component.css']
 })
-export class ClInputComponent implements OnInit {
+export class ClInputComponent implements OnInit, OnDestroy {
+
+  value: string;
+  class = '';
+  subscription: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.subscription = fromEvent(document, 'focusout').subscribe( data => {
+      if (this.value) {
+        this.class = 'has-value';
+      } else {
+        this.class = '';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
--- frontend/celso-lisboa/src/app/pages/login/login.module.ts
+++ frontend/celso-lisboa/src/app/pages/login/login.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import {LoginRoutingModule} from './login-routing.module';
-
+import {ClInputModule} from '../../../../projects/ui/src/lib/form-controls/cl-input/cl-input.module';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ClInputModule
   ]
 })
 export class LoginModule { }
