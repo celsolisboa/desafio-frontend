f51efbe4dd1555bf8f8cf2d09059560f70e8d814
Brendon Costa <43861825+brendon-costa@users.noreply.github.com>
2020-11-20 16:11:45 -0300

    feat(cl-input): create login page

--- frontend/celso-lisboa/src/app/app-routing.module.ts
+++ frontend/celso-lisboa/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
