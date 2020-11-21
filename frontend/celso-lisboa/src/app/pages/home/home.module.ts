36f610d6e54eb205266a00a54616e0e34a3427b7
Brendon Costa <43861825+brendon-costa@users.noreply.github.com>
2020-11-21 14:38:23 -0300

    feat(home): create navbar

--- frontend/celso-lisboa/src/app/pages/home/home.module.ts
+++ frontend/celso-lisboa/src/app/pages/home/home.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './component/home.component';
 import { HomeRoutingModule } from './home.routing.module';
-
+import {NavbarModule} from '../../../../projects/ui/src/lib/layout/navbar/navbar.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    NavbarModule
   ]
 })
 export class HomeModule { }
