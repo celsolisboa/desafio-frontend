54759ff5a88648a6431c8d12ee60c57ee410748f
Brendon Costa <43861825+brendon-costa@users.noreply.github.com>
2020-11-21 15:48:20 -0300

    feat(home): create layout navbar

--- frontend/celso-lisboa/projects/ui/src/lib/layout/cl-button/cl-button.module.ts
+++ frontend/celso-lisboa/projects/ui/src/lib/layout/cl-button/cl-button.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClButtonComponent } from './cl-button.component';
 
-
-
 @NgModule({
   declarations: [ClButtonComponent],
   imports: [
--- frontend/celso-lisboa/projects/ui/src/lib/layout/navbar/navbar.module.ts
+++ frontend/celso-lisboa/projects/ui/src/lib/layout/navbar/navbar.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
+import {ClButtonModule} from '../cl-button/cl-button.module';
 
 
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ClButtonModule
   ],
   exports: [NavbarComponent]
 })
