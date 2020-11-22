59b1a7c38fae2b874bce0b34e8d0e039dadf9e14
Brendon Costa <43861825+brendon-costa@users.noreply.github.com>
2020-11-22 16:17:43 -0300

    fix(auth): changed test name

--- frontend/celso-lisboa/src/app/core/auth/auth.guard.service.spec.ts
+++ frontend/celso-lisboa/src/app/core/auth/auth.guard.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Auth.GuardService } from './auth.guard.service';
+import { AuthGuardService } from './auth.guard.service';
 
 describe('Auth.GuardService', () => {
-  let service: Auth.GuardService;
+  let service: AuthGuardService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Auth.GuardService);
+    service = TestBed.inject(AuthGuardService);
   });
 
   it('should be created', () => {
